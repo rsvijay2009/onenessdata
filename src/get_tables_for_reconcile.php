1e4530110342be0d8fc3f0147d94c08d0bc8ebbc
Vijaykumar <rsvijay2009@gmail.com>
2024-06-09 20:27:42 +0530

    Reconsile design changes

--- src/get_tables_for_reconcile.php
+++ src/get_tables_for_reconcile.php
@@ -5,7 +5,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $tableName = $_POST['tableName'] ?? '';
     $selectBoxId = $_POST['selectBoxId'] ?? '';
     $selectBoxName = $_POST['selectBoxName'] ?? '';
-    $selectBoxButtonStyle = 'margin-top:20px; margin-left:20px; width:100%; color:#000; border: 1px solid #c9c5c5';
 }
 
 $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
@@ -16,10 +15,10 @@ $stmt->execute();
 $columns = $stmt->fetchAll(PDO::FETCH_COLUMN);
 
 
-$outputHtmlColumns = '<button class="btn dropdown-toggle" type="button" id="'.$selectBoxName.'" data-bs-toggle="dropdown" aria-expanded="false" style="margin-top:20px; margin-left:20px; width:100%; color:#000; border: 1px solid #c9c5c5;"><span style="margin-right: 310px;"> Select Columns</span></button><ul class="dropdown-menu" aria-labelledby="'.$selectBoxName.'" style="width: 95%;">';
+$outputHtmlColumns = '<button class="btn dropdown-toggle" type="button" id="'.$selectBoxName.'" data-bs-toggle="dropdown" aria-expanded="false" style="margin-left:-120px;color:#000; border: 1px solid #c9c5c5;width:237px;"><span style="margin-right: 90px;"> Select Columns</span></button><ul class="dropdown-menu" aria-labelledby="'.$selectBoxName.'" style="width: 95%; width:237px;">';
 
 foreach($columns as $column) {
-    $outputHtmlColumns.='<li><a class="dropdown-item"><input class="form-check-input column-checkbox" type="checkbox" name="'.$selectBoxName.'[]" value="'.$column.'" id="" style="margin-right: 10px;"><label class="form-check-label" for="checkbox">'.$column.'</label></a></li>';
+    $outputHtmlColumns.='<li><a class="dropdown-item"><input class="form-check-input column-checkbox tableColsChkBox" type="checkbox" name="'.$selectBoxName.'[]" value="'.$column.'" id="" style="margin-right: 10px;"><label class="form-check-label" for="checkbox">'.$column.'</label></a></li>';
 }
 
 $outputHtmlColumns.='</ul>';
--- src/reconcile.php
+++ src/reconcile.php
@@ -28,30 +28,37 @@ include_once "header.php";
                     <input type="hidden" id="successMsg" name="successMsg" value=<?= $successMsg ?>> -->
 
                     <div class="row" style="margin-top:50px;">
-                        <div class="col-xl-5">
-                            <div class="card" ondrop="drop(event)" ondragover="allowDrop(event)">
+                        <div class="col-xl-5" id="prepareDiv">
+                            <div class="card" ondrop="drop(event)" ondragover="allowDrop(event)" style="cursor:pointer;">
                                 <div class="plus-icon">Prepare</div>
                                 <div class="card-body"></div>
                             </div>
                         </div>
-                        <div class="col-xl-5" style="margin-left:150px;">
+                        <div class="col-xl-5" style="margin-left:150px;" id="compareDiv">
                             <div class="card" ondrop="drop(event)" ondragover="allowDrop(event)">
                                 <div class="plus-icon">Compare</div>
                                 <div class="card-body"></div>
                             </div>
                         </div>
                     </div>
-
-                    <div style="display:flex;">
-                        <select class='form-select' id='joinTable1' name='joinTable1' style="margin-left:20px;width:37.5%">
-                            <option value=''>Choose table</option>
-                            <?php foreach($tables as $table) {?>
-                                <option value='<?=$table['name']?>'><?=$table['name']?></option>
-                            <?php }?>
-                        </select>
+                    <div id="tableListDropDown" style="display:none;">
+                        <div style="display:flex;">
+                            <div  style="width:30%;">
+                                <select class='form-select' id='tablesList' name='tablesList' style="margin-left:20px;width:60%">
+                                    <option value=''>Choose table</option>
+                                    <?php foreach($tables as $table) {?>
+                                        <option value='<?=$table['name']?>'><?=$table['name']?></option>
+                                    <?php }?>
+                                </select>
+                            </div>
+                            <div style="">
+                                <div id="reconcileTableColumns1" name="reconcileTableColumns1" class="dropdown" style="display: flex; justify-content: flex-start;width:39.2%"></div>
+                        </div>
                     </div>
-                    <div style="display:flex;">
-                        <div id="reconcileTableColumns1" name="reconcileTableColumns1" class="dropdown" style="display: flex; justify-content: flex-start;width:39.2%">
+                    <div id="uniqueKeyGenerationTitleId" style="display:none; margin-left:20px;font-weight:bold;margin-top:50px;">Select columns for unique key generation
+                    </div>
+                    <div style="display:flex; display:none;" id="uniquKeyGenOptionsDiv">
+                        <div id="reconcileTableColumns2" name="reconcileTableColumns2" class="dropdown" style="display: flex; justify-content: flex-start;width:39.2%">
                         </div>
                     </div>
                     <!-- <p>
@@ -67,8 +74,8 @@ include_once "header.php";
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <script src="scripts/reconcile.js"></script>
 <script>
-$(document).ready(function(){
-    $('#joinTable1').change(function(e) {
+$(document).ready(function() {
+    $('#tablesList').change(function(e) {
         const tableName = e.target.value;
         $.ajax({
             type: "POST",
@@ -79,15 +86,43 @@ $(document).ready(function(){
                 selectBoxName: 'reconcileTableColumns1'
             },
             success: function(response) {
-                const result = response.split("||");
-                $("#reconcileTableColumns1").html(result[0]);
+                $("#reconcileTableColumns1").html(response);
+                $("#reconcileTableColumns2").html(response);
             },
             error: function(jqXHR, textStatus, errorThrown) {
                 console.log("Something went wrong");
             }
         });
     });
-    
+
+    $('#prepareDiv').click(function(){
+        $('#tableListDropDown').show();
+    });
+
+    $('#reconcileTableColumns1').on('change', '.tableColsChkBox', function() {
+        updateDropdown();
+    });
+
+    function updateDropdown() {
+        dropDownList = '<button class="btn dropdown-toggle" type="button" id="reconcileTableColumns1" data-bs-toggle="dropdown" aria-expanded="true" style="margin-top:20px; margin-left:20px; width:100%; color:#000; border: 1px solid #c9c5c5;"><span style="margin-right: 310px;"> Select Columns</span></button><ul class="dropdown-menu" aria-labelledby="reconcileTableColumns1" style="width: 95%; position: absolute; inset: 0px auto auto 0px; margin: 0px; transform: translate3d(20px, 60px, 0px);" data-popper-placement="bottom-start">';
+
+        const checkboxes = document.querySelectorAll('.tableColsChkBox');
+        // Collect selected checkboxes
+        const selectedOptions = [];
+        checkboxes.forEach(checkbox => {
+            if (checkbox.checked) {
+                selectedOptions.push(checkbox.value);
+            }
+        });
+
+        // Populate dropdown with selected options
+        selectedOptions.forEach(option => {
+            dropDownList+='<li><a class="dropdown-item"><input class="form-check-input column-checkbox" type="checkbox" name="uniqueKeyGenOptions[]" value="'+option+'" id="" style="margin-right: 10px;"><label class="form-check-label" for="checkbox">'+option+'</label></a></li>';
+        });
+        $('#uniqueKeyGenerationTitleId').show();
+        $('#uniquKeyGenOptionsDiv').show();
+        $('#reconcileTableColumns2').html(dropDownList);
+    }
 });
 </script>
 </body>

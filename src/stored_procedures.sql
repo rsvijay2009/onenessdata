494fbb9f6db8f0b976fe7ce3a8205fdc80a7f803
Vijaykumar <rsvijay2009@gmail.com>
2024-09-16 20:16:28 +0530

    Latest version of stored procedure

--- src/stored_procedures.sql
+++ src/stored_procedures.sql
@@ -1,91 +1,65 @@
-DROP PROCEDURE IF EXISTS GetDashboardData;
+DROP PROCEDURE IF EXISTS CompareTables;
 
-DELIMITER $$
-CREATE PROCEDURE `GetDashboardData`(IN dashboard_table_name VARCHAR(64), IN table_name VARCHAR(64))
+CREATE PROCEDURE CompareTables(
+    IN tableA VARCHAR(255),
+    IN tableB VARCHAR(255),
+    IN tableA_Relationship VARCHAR(255),
+    IN tableB_Relationship VARCHAR(255),
+    IN tableA_Column_To_Compare VARCHAR(255),
+    IN tableB_Column_To_Compare VARCHAR(255)
+)
 BEGIN
-    DECLARE data_quality_correct_data INT;
-    DECLARE data_quality_incorrect_data INT;
-    DECLARE text_issue INT;
-    DECLARE number_issue INT;
-    DECLARE date_issue INT;
-    DECLARE alphanumeric_issue INT;
-    DECLARE email_issue INT;
-    DECLARE duplicate_entries_issue INT;
-    DECLARE others_issue INT;
-    DECLARE null_issue INT;
-    DECLARE overall_correct_data INT;
-    DECLARE overall_incorrect_data INT;
-    DECLARE verification_table_name VARCHAR(64);
-    DECLARE sql_query TEXT;
+    DECLARE timestamp_str VARCHAR(20);
+    DECLARE compare_table_name VARCHAR(255);
+    DECLARE create_table_sql TEXT;
+    DECLARE insert_sql TEXT;
+    DECLARE select_sql TEXT;
 
-    -- Concatenate '_data_verification' to the table name
-    SET verification_table_name = CONCAT(table_name, '_data_verification');
+    -- Generate unique table name using timestamp
+    SET timestamp_str = DATE_FORMAT(NOW(), '%Y%m%d%H%i%s');
+    SET compare_table_name = CONCAT('compare_data_', timestamp_str);
 
-    -- Construct the dynamic SQL query to get 'others_issue' count
-    SET @sql_query_others = CONCAT('SELECT COUNT(*) INTO @others_issue FROM ', verification_table_name, ' WHERE  ignore_flag = 0 AND remarks = ''Others''');
-    
-    -- Prepare and execute the dynamic SQL for 'Others' count
-    PREPARE stmt_others FROM @sql_query_others;
-    EXECUTE stmt_others;
-    DEALLOCATE PREPARE stmt_others;
-    
-    -- Assign the others issue count
-    SET others_issue = @others_issue;
+    -- Create the comparison table
+    SET @create_table_sql = CONCAT(
+        'CREATE TABLE ', compare_table_name, ' (',
+        'relationship_key VARCHAR(255), ',
+        'tableA_', tableA_Relationship, ' VARCHAR(255), ',
+        'tableB_', tableB_Relationship, ' VARCHAR(255), ',
+        'difference VARCHAR(255))'
+    );
 
-    -- Construct the dynamic SQL query to get 'null_issue' count
-    SET @sql_query_null = CONCAT('SELECT COUNT(*) INTO @null_issue FROM ', verification_table_name, ' WHERE ignore_flag = 0 AND (remarks = ''NULL'' OR remarks IS NULL)');
-    
-    -- Prepare and execute the dynamic SQL for 'Null' count
-    PREPARE stmt_null FROM @sql_query_null;
-    EXECUTE stmt_null;
-    DEALLOCATE PREPARE stmt_null;
-    
-    -- Assign the null issue count
-    SET null_issue = @null_issue;
-
-    -- Construct the dynamic SQL query for the other metrics
-    SET @sql_query = CONCAT('SELECT data_quality_correct_data, data_quality_incorrect_data, text_issue, number_issue, date_issue, alphanumeric_issue, email_issue, duplicate_entries_issue, overall_correct_data, overall_incorrect_data ',
-                            'INTO @data_quality_correct_data, @data_quality_incorrect_data, @text_issue, @number_issue, @date_issue, @alphanumeric_issue, @email_issue, @duplicate_entries_issue, @overall_correct_data, @overall_incorrect_data ',
-                            'FROM ', dashboard_table_name, ' LIMIT 1');
-
-    -- Prepare and execute the dynamic SQL for the main metrics
-    PREPARE stmt FROM @sql_query;
+    -- Execute the create table SQL
+    PREPARE stmt FROM @create_table_sql;
     EXECUTE stmt;
     DEALLOCATE PREPARE stmt;
 
-    -- Assign the variables from the prepared statement execution
-    SET data_quality_correct_data = @data_quality_correct_data;
-    SET data_quality_incorrect_data = @data_quality_incorrect_data;
-    SET text_issue = @text_issue;
-    SET number_issue = @number_issue;
-    SET date_issue = @date_issue;
-    SET alphanumeric_issue = @alphanumeric_issue;
-    SET email_issue = @email_issue;
-    SET duplicate_entries_issue = @duplicate_entries_issue;
-    SET overall_correct_data = @overall_correct_data;
-    SET overall_incorrect_data = @overall_incorrect_data;
+    -- Construct the insert query
+    SET @insert_sql = CONCAT(
+        'INSERT INTO ', compare_table_name, ' (relationship_key) ',
+        'SELECT DISTINCT ', tableA_Relationship, ' FROM ', tableA, ' ',
+        'UNION ',
+        'SELECT DISTINCT ', tableB_Relationship, ' FROM ', tableB
+    );
 
-    -- Return the values
-    SELECT data_quality_correct_data AS 'data_quality_correct_data', 
-           data_quality_incorrect_data AS 'data_quality_incorrect_data',
-           text_issue AS 'text_issue',
-           number_issue AS 'number_issue',
-           date_issue AS 'date_issue',
-           alphanumeric_issue AS 'alphanumeric_issue',
-           email_issue AS 'email_issue',
-           duplicate_entries_issue AS 'duplicate_entries_issue',
-           others_issue AS 'others_issue',
-           null_issue AS 'null_issue',
-           overall_correct_data AS 'overall_correct_data',
-           overall_incorrect_data AS 'overall_incorrect_data';
-END $$
-DELIMITER ;
+    -- Execute the insert SQL
+    PREPARE stmt FROM @insert_sql;
+    EXECUTE stmt;
+    DEALLOCATE PREPARE stmt;
+    
+    -- Construct the select query
+    SET select_sql = CONCAT('SELECT * FROM ', compare_table_name);
+
+    -- Execute the select SQL and return the results
+    SET @select_sql = select_sql;
+    PREPARE stmt FROM @select_sql;
+    EXECUTE stmt;
+    DEALLOCATE PREPARE stmt;
+END
 
 
 DROP PROCEDURE IF EXISTS DeleteProjectData;
 
-DELIMITER $$
-CREATE PROCEDURE `DeleteProjectData`(IN project_id INT)
+CREATE PROCEDURE DeleteProjectData (IN project_id INT)
 BEGIN
     DECLARE done INT DEFAULT 0;
     DECLARE base_table_name VARCHAR(255);
@@ -93,6 +67,7 @@ BEGIN
     DECLARE table2_name VARCHAR(255);
     DECLARE table3_name VARCHAR(255);
     
+
     DECLARE cur CURSOR FOR 
         SELECT `name` 
         FROM tables_list 
@@ -141,21 +116,14 @@ BEGIN
     END LOOP;
 
     CLOSE cur;
-
-     -- Delete the record from projects table
+    -- Delete the record from projects table
     DELETE FROM projects WHERE id = project_id;
-
-END$$
-
-DELIMITER ;
-
-
+END
 
 
 DROP PROCEDURE IF EXISTS DropAndCleanUpTable;
 
-DELIMITER $$
-CREATE PROCEDURE `DropAndCleanUpTable`(IN tableId INT)
+CREATE PROCEDURE DropAndCleanUpTable(IN tableId INT)
 BEGIN
     DECLARE _tableName VARCHAR(255);
     DECLARE _dataTypeTableName VARCHAR(255);
@@ -215,77 +183,12 @@ BEGIN
 
     -- Delete the entry from tables_list
     DELETE FROM tables_list WHERE id = tableId;
-END$$
-
-DELIMITER ;
-
-
-DROP PROCEDURE IF EXISTS CompareTables;
-
-DELIMITER $$
-CREATE PROCEDURE `CompareTables`(
-    IN tableA VARCHAR(255),
-    IN tableB VARCHAR(255),
-    IN tableA_Relationship VARCHAR(255),
-    IN tableB_Relationship VARCHAR(255),
-    IN tableA_Column_To_Compare VARCHAR(255),
-    IN tableB_Column_To_Compare VARCHAR(255)
-)
-BEGIN
-    DECLARE timestamp_str VARCHAR(20);
-    DECLARE compare_table_name VARCHAR(255);
-    DECLARE create_table_sql TEXT;
-    DECLARE insert_sql TEXT;
-    DECLARE select_sql TEXT;
-
-    -- Generate unique table name using timestamp
-    SET timestamp_str = DATE_FORMAT(NOW(), '%Y%m%d%H%i%s');
-    SET compare_table_name = CONCAT('compare_data_', timestamp_str);
-
-    -- Create the comparison table
-    SET @create_table_sql = CONCAT(
-        'CREATE TABLE ', compare_table_name, ' (',
-        'relationship_key VARCHAR(255), ',
-        'tableA_', tableA_Relationship, ' VARCHAR(255), ',
-        'tableB_', tableB_Relationship, ' VARCHAR(255), ',
-        'difference VARCHAR(255))'
-    );
-
-    -- Execute the create table SQL
-    PREPARE stmt FROM @create_table_sql;
-    EXECUTE stmt;
-    DEALLOCATE PREPARE stmt;
-
-    -- Construct the insert query
-    SET @insert_sql = CONCAT(
-        'INSERT INTO ', compare_table_name, ' (relationship_key) ',
-        'SELECT DISTINCT ', tableA_Relationship, ' FROM ', tableA, ' ',
-        'UNION ',
-        'SELECT DISTINCT ', tableB_Relationship, ' FROM ', tableB
-    );
-
-    -- Execute the insert SQL
-    PREPARE stmt FROM @insert_sql;
-    EXECUTE stmt;
-    DEALLOCATE PREPARE stmt;
-
-    -- Construct the select query
-    SET select_sql = CONCAT('SELECT * FROM ', compare_table_name);
-
-    -- Execute the select SQL and return the results
-    SET @select_sql = select_sql;
-    PREPARE stmt FROM @select_sql;
-    EXECUTE stmt;
-    DEALLOCATE PREPARE stmt;
-END $$
-DELIMITER ;
+END
 
 
 DROP PROCEDURE IF EXISTS FindIncorrectData;
--- Find Incorrect data stored procedure
 
-DELIMITER $$
-CREATE PROCEDURE `FindIncorrectData`(IN tbl_name VARCHAR(255), IN datatype_tbl_name VARCHAR(255))
+CREATE PROCEDURE FindIncorrectData (IN tbl_name VARCHAR(255), IN datatype_tbl_name VARCHAR(255))
 BEGIN
     DECLARE done INT DEFAULT FALSE;
     DECLARE col_name VARCHAR(255);
@@ -440,6 +343,86 @@ BEGIN
 
     -- Close the cursor
     CLOSE cur;
+END
 
-END $$
-DELIMITER ;
+
+DROP PROCEDURE IF EXISTS GetDashboardData;
+
+CREATE PROCEDURE GetDashboardData (IN dashboard_table_name VARCHAR(64), IN table_name VARCHAR(64))
+BEGIN
+    DECLARE data_quality_correct_data INT;
+    DECLARE data_quality_incorrect_data INT;
+    DECLARE text_issue INT;
+    DECLARE number_issue INT;
+    DECLARE date_issue INT;
+    DECLARE alphanumeric_issue INT;
+    DECLARE email_issue INT;
+    DECLARE duplicate_entries_issue INT;
+    DECLARE others_issue INT;
+    DECLARE null_issue INT;
+    DECLARE overall_correct_data INT;
+    DECLARE overall_incorrect_data INT;
+    DECLARE verification_table_name VARCHAR(64);
+    DECLARE sql_query TEXT;
+
+    -- Concatenate '_data_verification' to the table name
+    SET verification_table_name = CONCAT(table_name, '_data_verification');
+
+    -- Construct the dynamic SQL query to get 'others_issue' count
+    SET @sql_query_others = CONCAT('SELECT COUNT(*) INTO @others_issue FROM ', verification_table_name, ' WHERE  ignore_flag = 0 AND remarks = ''Others''');
+
+    -- Prepare and execute the dynamic SQL for 'Others' count
+    PREPARE stmt_others FROM @sql_query_others;
+    EXECUTE stmt_others;
+    DEALLOCATE PREPARE stmt_others;
+
+    -- Assign the others issue count
+    SET others_issue = @others_issue;
+
+    -- Construct the dynamic SQL query to get 'null_issue' count
+    SET @sql_query_null = CONCAT('SELECT COUNT(*) INTO @null_issue FROM ', verification_table_name, ' WHERE ignore_flag = 0 AND (remarks = ''NULL'' OR remarks IS NULL)');
+
+    -- Prepare and execute the dynamic SQL for 'Null' count
+    PREPARE stmt_null FROM @sql_query_null;
+    EXECUTE stmt_null;
+    DEALLOCATE PREPARE stmt_null;
+
+    -- Assign the null issue count
+    SET null_issue = @null_issue;
+
+    -- Construct the dynamic SQL query for the other metrics
+    SET @sql_query = CONCAT('SELECT data_quality_correct_data, data_quality_incorrect_data, text_issue, number_issue, date_issue, alphanumeric_issue, email_issue, duplicate_entries_issue, overall_correct_data, overall_incorrect_data ',
+                            'INTO @data_quality_correct_data, @data_quality_incorrect_data, @text_issue, @number_issue, @date_issue, @alphanumeric_issue, @email_issue, @duplicate_entries_issue, @overall_correct_data, @overall_incorrect_data ',
+                            'FROM ', dashboard_table_name, ' LIMIT 1');
+
+    -- Prepare and execute the dynamic SQL for the main metrics
+    PREPARE stmt FROM @sql_query;
+    EXECUTE stmt;
+    DEALLOCATE PREPARE stmt;
+
+    -- Assign the variables from the prepared statement execution
+    SET data_quality_correct_data = @data_quality_correct_data;
+    SET data_quality_incorrect_data = @data_quality_incorrect_data;
+    SET text_issue = @text_issue;
+    SET number_issue = @number_issue;
+    SET date_issue = @date_issue;
+    SET alphanumeric_issue = @alphanumeric_issue;
+    SET email_issue = @email_issue;
+    SET duplicate_entries_issue = @duplicate_entries_issue;
+    SET overall_correct_data = @overall_correct_data;
+    SET overall_incorrect_data = @overall_incorrect_data;
+
+    -- Return the values
+    SELECT data_quality_correct_data AS 'data_quality_correct_data',
+           data_quality_incorrect_data AS 'data_quality_incorrect_data',
+           text_issue AS 'text_issue',
+           number_issue AS 'number_issue',
+           date_issue AS 'date_issue',
+           alphanumeric_issue AS 'alphanumeric_issue',
+           email_issue AS 'email_issue',
+           duplicate_entries_issue AS 'duplicate_entries_issue',
+           others_issue AS 'others_issue',
+           null_issue AS 'null_issue',
+           overall_correct_data AS 'overall_correct_data',
+           overall_incorrect_data AS 'overall_incorrect_data';
+END
